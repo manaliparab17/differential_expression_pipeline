f07ae03258523e7943a9fffe9f9c5eed58be86fc
manaliparab17 <manali.parab1706@gmail.com>
2026-08-03 13:46:44 +0530

    Update pca_plot.R

--- pca_plot.R
+++ pca_plot.R
@@ -1,12 +1,5 @@
 #!/usr/bin/env Rscript
 
-# Rscript pca_plot.R <normalized_counts.csv> <output_folder>
-#
-# Makes two plots from the normalized counts:
-#   1. PCA scatter (PC1 vs PC2) - shows which samples look similar/different
-#   2. Bar chart of total library size per sample
-# Useful for spotting outlier samples before trusting DE results.
-# Uses base R only, no extra packages needed.
 
 args <- commandArgs(trailingOnly = TRUE)
 counts_file <- args[1]
@@ -14,26 +7,20 @@ output_folder <- args[2]
 
 dir.create(output_folder, showWarnings = FALSE)
 
-# read normalized counts (genes as rows, samples as columns)
 counts <- read.csv(counts_file, row.names = 1, check.names = FALSE)
 counts <- as.matrix(counts)
 
-# label each sample with its group and time (edit this to match your samples)
 sample_names <- colnames(counts)
 sample_labels <- c("placebo_pre", "placebo_post", "mife_pre", "mife_post")
 sample_colors <- c("#2a78d6", "#2a78d6", "#eb6834", "#eb6834")
 
-# log transform (log2 + 1 so we don't take log of zero)
 log_counts <- log2(counts + 1)
 
-# run PCA
-# t() because prcomp expects samples as rows, genes as columns
+
 pca <- prcomp(t(log_counts))
 
-# how much variance each PC explains
 var_explained <- round((pca$sdev^2 / sum(pca$sdev^2)) * 100, 1)
 
-# --- plot 1: PCA scatter ---
 png(file.path(output_folder, "pca_plot.png"), width = 700, height = 600)
 
 plot(pca$x[, 1], pca$x[, 2],
@@ -49,7 +36,6 @@ legend("topright", legend = c("placebo", "mifepristone"),
 
 dev.off()
 
-# --- plot 2: library size bar chart ---
 lib_sizes <- colSums(counts)
 
 png(file.path(output_folder, "library_size.png"), width = 700, height = 500)
